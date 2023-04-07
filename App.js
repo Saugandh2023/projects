675ceb1ef9f9b7cd22d8825a4dc70f4da1fa7e7b
Saugandh Sonawane <125334953+Saugandh2023@users.noreply.github.com>
2023-04-07 14:48:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Component } from 'react';
+import Car from './component';
+
+function App() {
+  return (
+  <Car>
+        
+    </Car>
+  );
+}
+
+export default App;
